b3d008cb44a77ba4ebfe44fbbfcdcab5833e5be0
development@khanon3d.com <development@khanon3d.com>
2023-06-15 22:32:55 +0200

    .

--- pipeline/create-version-tag.js
+++ pipeline/create-version-tag.js
@@ -7,7 +7,7 @@ const tag = `v${localVersion}`
 console.log(`Adding Git tag '${tag}'`)
 
 let buffer
-buffer = require('child_process').execSync('git switch main')
+buffer = require('child_process').execSync('git switch eda26bcaf4ceb24858910ee8948a10168ddbb1c4')
 console.log('aki result A:', buffer.toString())
 buffer = require('child_process').execSync('git show-ref')
 console.log('aki result B:', buffer.toString())
