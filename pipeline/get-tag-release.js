599ffe5755b38bfeae5b8c2ba5a16f1601035b26
development@khanon3d.com <development@khanon3d.com>
2023-06-18 14:48:47 +0200

    .

--- pipeline/get-tag-release.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const localPackage = require('../package.json')
-const localVersion = localPackage.version
-const tag = `v${localVersion}`
-
-console.log(tag)
