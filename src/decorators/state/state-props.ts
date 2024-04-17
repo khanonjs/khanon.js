3b5006efed8f0e23f55c385444a92374920965ef
development@khanon3d.com <development@khanon3d.com>
2024-04-17 18:55:24 +0200

    .

--- src/decorators/state/state-props.ts
+++ src/decorators/state/state-props.ts
@@ -1,5 +1,5 @@
 import { CameraConstructor } from '../../constructors'
 
 export interface StateProps {
-  camera: CameraConstructor
+  camera: CameraConstructor // 8a8f sure we want to set a new camera per state? SceneState vs ActorState?
 }
