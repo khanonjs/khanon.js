974d2c9534d8c2bd39f831844e08ba4773e05bac
development@khanon3d.com <development@khanon3d.com>
2024-06-08 23:56:44 +0200

    .

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -55,7 +55,7 @@ export function objectToString(data: unknown, stringfy = true): string {
 }
 
 /**
- * Removes duplicated items in array or first level of object properties
+ * Removes duplicated items in array or first level object array
  */
 export function removeArrayDuplicitiesInObject<T extends object>(values: T): T {
   function remove(vals: any): any {
