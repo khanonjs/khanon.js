abc7e7ad566b629d56dbbaf83329c978388053b9
development@khanon3d.com <development@khanon3d.com>
2024-07-27 20:50:47 +0200

    .

--- src/types/notificable-type.ts
+++ src/types/notificable-type.ts
@@ -1,19 +1,12 @@
 import { ActorInterface } from '../decorators/actor'
-import { ActorActionInterface } from '../decorators/actor/actor-action'
 import { ActorStateInterface } from '../decorators/actor/actor-state'
 import { AppInterface } from '../decorators/app'
-import { MeshInterface } from '../decorators/mesh'
 import { SceneInterface } from '../decorators/scene'
-import { SceneActionInterface } from '../decorators/scene/scene-action'
 import { SceneStateInterface } from '../decorators/scene/scene-state'
-import { SpriteInterface } from '../decorators/sprite'
 
 export type NotificableType =
   AppInterface |
-  ActorInterface<SpriteInterface> | ActorInterface<SpriteInterface>[] |
-  ActorInterface<MeshInterface> | ActorInterface<MeshInterface>[] |
+  ActorInterface<any> | ActorInterface<any>[] |
   ActorStateInterface | ActorStateInterface[] |
-  ActorActionInterface | ActorActionInterface[] |
   SceneInterface | SceneInterface[] |
-  SceneStateInterface | SceneStateInterface[] |
-  SceneActionInterface | SceneActionInterface[]
+  SceneStateInterface | SceneStateInterface[]
