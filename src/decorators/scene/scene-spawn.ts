4cb239c55163ae9d601c26751695d2ff75df29e1
development@khanon3d.com <development@khanon3d.com>
2024-08-08 21:05:20 +0200

    .

--- src/decorators/scene/scene-spawn.ts
+++ src/decorators/scene/scene-spawn.ts
@@ -12,7 +12,8 @@ import { SpriteInterface } from '../sprite/sprite-interface'
 import { SceneInterface } from './scene-interface'
 
 // TODO add support to inject a user defined SceneSpawn class into the scene?
-// TODO add counter argument in methods, to spawn many elements in a single step. 'index' would be sent to 'onSpawn' Actor method.
+// TODO add counter argument in methods, to spawn many elements in a single step. 'index' would be sent to 'onSpawn' methods.
+// TODO add 'aggregateSpawnMethod' as argument to combine with 'onSpawn'.
 export class SceneSpawn {
   private readonly scene?: SceneInterface
   private readonly scenePrototype?: any
