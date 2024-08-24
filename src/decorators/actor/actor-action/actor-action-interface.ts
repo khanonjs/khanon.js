9ed049d89b708d64210a56ba0674d3032a1f086f
development@khanon3d.com <development@khanon3d.com>
2024-08-24 18:51:38 +0200

    .

--- src/decorators/actor/actor-action/actor-action-interface.ts
+++ src/decorators/actor/actor-action/actor-action-interface.ts
@@ -1,18 +1,39 @@
 import * as BABYLON from '@babylonjs/core'
 
 import { ActionInterface } from '../../../base'
+import { Metadata } from '../../../base/interfaces/metadata/metadata'
+import { Rect } from '../../../models/rect'
 import { MeshInterface } from '../../mesh'
+import { SceneInterface } from '../../scene/scene-interface'
 import { SpriteInterface } from '../../sprite'
 import { ActorInterface } from '../actor-interface'
 import { ActorActionProps } from './actor-action-props'
 
-export abstract class ActorActionInterface<S = any, A = ActorInterface<SpriteInterface | MeshInterface>> extends ActionInterface<S> {
+export abstract class ActorActionInterface<S = any, A = ActorInterface<SpriteInterface | MeshInterface>> implements ActionInterface<S> {
   abstract props?: ActorActionProps
   abstract countFramesUpdate$?: BABYLON.Observer<number>
   abstract countFrames?: number
+  abstract metadata?: Metadata
+  abstract loopUpdate$?: BABYLON.Observer<number>
+  abstract canvasResize$?: BABYLON.Observer<Rect>
+  abstract start?(setup: S): void
+  abstract end?(): void
 
   /**
    * User available
    */
   abstract actor: A
+  abstract setup: S
+  abstract set loopUpdate(value: boolean)
+  abstract get loopUpdate(): boolean
+  abstract get scene(): SceneInterface
+  abstract stop(): void // Callable from user Action, it will call to 'owner.stopActionFromInstance', then owner calls 'action.end' after remove it.
+
+  /**
+   * User defined optional
+   */
+  onPlay?(): void
+  onStop?(): void
+  onLoopUpdate?(delta: number): void
+  onCanvasResize?(size: Rect): void
 }
