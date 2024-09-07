2e3183774b39ebad116536076b4d65bb429ba938
development@khanon3d.com <development@khanon3d.com>
2024-09-07 22:00:49 +0200

    .

--- tutorials/blank-project/public/app.js
+++ tutorials/blank-project/public/app.js
@@ -1,27 +1,28 @@
 "use strict";
-// import {
-//   App,
-//   AppInterface,
-//   KJS,
-//   Logger
-// } from '@khanonjs/engine'
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LPWebsite = void 0;
+const tslib_1 = require("tslib");
+const engine_1 = require("@khanonjs/engine");
 console.log('aki STARTING APP???');
-// @App({
-//   name: 'Khanon.js blank project',
-//   loopUpdate: {
-//     fps: 165
-//   }
-// })
-// export class LPWebsite extends AppInterface {
-//   onStart() {
-//     // Entrypoint of your application
-//     Logger.trace('App onStart')
-//   }
-//   onClose() {
-//     Logger.trace('App onClose')
-//   }
-//   onError(error?: string) {
-//     Logger.error('App onError:', error)
-//   }
-// }
+let LPWebsite = class LPWebsite extends engine_1.AppInterface {
+    onStart() {
+        // Entrypoint of your application
+        engine_1.Logger.trace('App onStart');
+    }
+    onClose() {
+        engine_1.Logger.trace('App onClose');
+    }
+    onError(error) {
+        engine_1.Logger.error('App onError:', error);
+    }
+};
+exports.LPWebsite = LPWebsite;
+exports.LPWebsite = LPWebsite = tslib_1.__decorate([
+    (0, engine_1.App)({
+        name: 'Khanon.js blank project',
+        loopUpdate: {
+            fps: 165
+        }
+    })
+], LPWebsite);
 //# sourceMappingURL=app.js.map
--- tutorials/blank-project/src/app.ts
+++ tutorials/blank-project/src/app.ts
@@ -1,29 +1,29 @@
-// import {
-//   App,
-//   AppInterface,
-//   KJS,
-//   Logger
-// } from '@khanonjs/engine'
+import {
+  App,
+  AppInterface,
+  KJS,
+  Logger
+} from '@khanonjs/engine'
 
 console.log('aki STARTING APP???')
 
-// @App({
-//   name: 'Khanon.js blank project',
-//   loopUpdate: {
-//     fps: 165
-//   }
-// })
-// export class LPWebsite extends AppInterface {
-//   onStart() {
-//     // Entrypoint of your application
-//     Logger.trace('App onStart')
-//   }
+@App({
+  name: 'Khanon.js blank project',
+  loopUpdate: {
+    fps: 165
+  }
+})
+export class LPWebsite extends AppInterface {
+  onStart() {
+    // Entrypoint of your application
+    Logger.trace('App onStart')
+  }
 
-//   onClose() {
-//     Logger.trace('App onClose')
-//   }
+  onClose() {
+    Logger.trace('App onClose')
+  }
 
-//   onError(error?: string) {
-//     Logger.error('App onError:', error)
-//   }
-// }
+  onError(error?: string) {
+    Logger.error('App onError:', error)
+  }
+}
