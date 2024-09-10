f5a507bfd97466f5f864f6145d9c645d5d0ec646
development@khanon3d.com <development@khanon3d.com>
2024-09-10 20:50:49 +0200

    .

--- src/kjs/index.d.ts
+++ src/kjs/index.d.ts
@@ -63,7 +63,7 @@ export declare namespace KJS {
   /**
    * Sets the app state.
    */
-  export function startAppState<S extends AppStateConstructor>(state: S, setup: InstanceType<S>['setup']): AppStateInterface // TODO is it possible to make 'setup' argument optional whether InstanceType<S>['setup'] type is 'any'?
+  export function switchAppState<S extends AppStateConstructor>(state: S, setup: InstanceType<S>['setup']): AppStateInterface // TODO is it possible to make 'setup' argument optional whether InstanceType<S>['setup'] type is 'any'?
 
   /**
    * Clears cache.
