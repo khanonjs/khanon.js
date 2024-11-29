00542b18809b447c45b4b104d3fb6ef5d8b62c1f
development@khanon3d.com <development@khanon3d.com>
2024-11-29 20:13:30 +0100

    Fix

--- src/decorators/gui/gui-decorator.ts
+++ src/decorators/gui/gui-decorator.ts
@@ -1,11 +1,19 @@
+import { LoadingProgress } from '../../base'
+import { SceneInterface } from '../scene/scene-interface'
 import { GUICore } from './gui-core'
 import { GUIProps } from './gui-props'
-import { GUIType } from './gui-type'
 
 export function GUI(props: GUIProps): any {
-  return function <T extends { new (...args: any[]): GUIType }>(constructor: T & GUIType, context: ClassDecoratorContext) {
+  return function <T extends { new (...args: any[]): GUICore }>(constructor: T & GUICore, context: ClassDecoratorContext) {
     const _class = class extends constructor implements GUICore {
       props = props
+      load(owner?: SceneInterface): LoadingProgress {
+        return null as any
+      }
+
+      unload(owner?: SceneInterface): void {
+
+      }
     }
     return _class
   }
