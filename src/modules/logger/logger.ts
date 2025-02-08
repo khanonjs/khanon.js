b6e486a9f2bcd94b7cec5ac39e16f56e2a8a7a33
development@khanon3d.com <development@khanon3d.com>
2025-02-08 21:03:41 +0100

    Enhance Logger: Use objectToString for parameter logging in debug methods

--- src/modules/logger/logger.ts
+++ src/modules/logger/logger.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-useless-call */
+import { objectToString } from '../../utils/utils'
 import { LoggerLevels } from './logger-levels'
 
 export class Logger {
@@ -26,22 +27,22 @@ export class Logger {
     }
   }
 
-  // TODO: Add posibility to hide a debug log depending on an Id (so there's an Id list that the user can modify)
+  // TODO: Add posibility to hide a debug log depending on an Id (so there's an Id list that the user can supply)
   static debug(msg: string, ...params: any[]) {
     if (Logger.level >= LoggerLevels.DEBUG) {
-      console.log.apply(console, ['%c' + msg, 'background: #d7ffd6; color: #014001;' + Logger.boldStyle, ...params])
+      console.log.apply(console, ['%c' + msg, 'background: #d7ffd6; color: #014001;' + Logger.boldStyle, ...params.map(param => objectToString(param))])
     }
   }
 
   static debugError(msg: string, ...params: any[]) {
     if (Logger.level >= LoggerLevels.DEBUG) {
-      console.log.apply(console, ['%c' + msg, 'background: #ffc2c2; color: #550000;' + Logger.boldStyle, ...params])
+      console.log.apply(console, ['%c' + msg, 'background: #ffc2c2; color: #550000;' + Logger.boldStyle, ...params.map(param => objectToString(param))])
     }
   }
 
   static trace(msg: string, ...params: any[]) {
     if (Logger.level >= LoggerLevels.TRACE) {
-      console.log.apply(console, ['%c' + msg, 'background: #e010e0; color: #240524;' + Logger.boldStyle, ...params])
+      console.log.apply(console, ['%c' + msg, 'background: #e010e0; color: #240524;' + Logger.boldStyle, ...params.map(param => objectToString(param))])
     }
   }
 }
