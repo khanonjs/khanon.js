57a721e1b455a2decf2743879e8f7a5b11825192
development@khanon3d.com <development@khanon3d.com>
2025-10-14 19:41:03 +0200

    .

--- src/types/engine-configuration.d.ts
+++ src/types/engine-configuration.d.ts
@@ -1,18 +1,28 @@
 import { EngineOptions } from '@babylonjs/core/Engines/thinEngine'
+import { WebGPUEngineOptions } from '@babylonjs/core/Engines/webgpuEngine'
+
+import { EngineMode } from './engine-mode'
 
 export type EngineConfiguration = {
   /**
-   * Default = false
+   * Default = false. Not used in WebGPU mode.
    */
   antialias?: boolean
 
   /**
-   * Refers to Babylon engine options: https://doc.babylonjs.com/typedoc/interfaces/BABYLON.EngineOptions
-   */
-  options?: EngineOptions
-
-  /**
-   * Default = true
+   * Default = true. Not used in WebGPU mode.
    */
   adaptToDeviceRatio?: boolean
+
+  /**
+   * Refers to:
+   * - Mode WebGL:  https://doc.babylonjs.com/typedoc/interfaces/BABYLON.EngineOptions
+   * - Mode WebGPU: https://doc.babylonjs.com/typedoc/interfaces/BABYLON.WebGPUEngineOptions
+   */
+  options?: EngineOptions | WebGPUEngineOptions
+
+  /**
+   * Engine mode (WebGPU / WebGL). Default = WebGL.
+   */
+  mode?: EngineMode
 }
